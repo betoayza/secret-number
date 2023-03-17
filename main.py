5c722b96ab3015ff577fb13bca6ebdaed27e42fb
betoayza <aaayza@gmail.com>
2023-03-17 14:40:56 -0300

    First commit

--- main.py
+++ main.py
@@ -11,12 +11,14 @@ print("Welcome to Secret-Number!\n")
 while chosen_number != secret_number and chosen_number != -1:
     try:
         if isFirtRun:
-            chosen_number = int(input("Enter a number between 1 and 100 [-1 to exit].: "))
+            chosen_number = int(
+                input("Enter a number between 1 and 100 [-1 to exit].: "))
             isFirtRun = False
         else:
-            chosen_number = int(input("Ups! That's was so close, try again [-1 to exit]: "))
+            chosen_number = int(
+                input("Ups! That's was so close, try again [-1 to exit]: "))
     except ValueError:
-        print("That's not a number, ha!")        
+        print("That's not a number, ha!")
 
 if chosen_number == -1:
     print("You were defeated by your impatience. Good luck for next time!")
